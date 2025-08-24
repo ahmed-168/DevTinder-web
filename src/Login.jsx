4276c76ab5d359a078a5744be11023f29ab3c674
ahmedsubhani168@gmail.com <ahmedsubhani168@gmail.com>
2025-08-24 22:32:37 +0300

    created the login form

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex items-center justify-center py-32">
+      <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4 ">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="btn btn-primary mt-4">Login</button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
